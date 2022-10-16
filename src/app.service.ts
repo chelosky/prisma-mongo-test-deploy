b952cf9a44d87f0dc1dc6286ab3063e4808ee2a6
Chelosky <marcelolazo19972013@hotmail.com>
2022-10-16 02:01:09 -0300

    xd 123

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ export class AppService {
   constructor(private readonly prismaService: PrismaService) {}
 
   getHello(): string {
-    return 'Hello World! v3';
+    return 'Hello World! v4.1';
   }
 
   async getPosts() {
