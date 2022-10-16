25d04d77885e371491df18bafffd12e1d3398ac1
Chelosky <marcelolazo19972013@hotmail.com>
2022-10-16 01:10:06 -0300

    xd 123

--- src/modules/food/food.service.spec.ts
+++ src/modules/food/food.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../../prisma/prisma.service';
+import { CategoryService } from '../category/category.service';
 import { FoodService } from './food.service';
 
 describe('FoodService', () => {
@@ -6,7 +8,17 @@ describe('FoodService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FoodService],
+      providers: [
+        FoodService,
+        {
+          provide: CategoryService,
+          useValue: {},
+        },
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<FoodService>(FoodService);
